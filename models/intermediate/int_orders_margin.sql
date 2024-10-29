e473733a1a7e90af885cdf888802176868b7fa1c
baptiste.lewagondata@gmail.com <baptiste.lewagondata@gmail.com>
2024-10-29 13:44:00 +0000

    Created int_operational_margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,9 +1,10 @@
 SELECT
     sales.orders_id,
     sales.date_date,
-    ROUND(SUM(sales.revenue),2),
-    ROUND(SUM(sales.quantity),2),
-    ROUND(SUM(sales.purchase_cost),2),
-    ROUND(SUM(sales.margin),2)
+    ROUND(SUM(sales.revenue),2) AS revenue,
+    ROUND(SUM(sales.quantity),2) AS quantity,
+    ROUND(SUM(sales.purchase_cost),2) AS purchase_cost,
+    ROUND(SUM(sales.margin),2) AS margin
 FROM {{ ref('int_sales_margin') }} AS sales
 GROUP BY orders_id, date_date
+ORDER BY orders_id DESC
