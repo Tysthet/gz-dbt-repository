f06ec468fad3d684c39d7029f2f6f9c3e0f256b7
baptiste.lewagondata@gmail.com <baptiste.lewagondata@gmail.com>
2024-10-29 14:00:15 +0000

    Validated int_orders_operational

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,7 @@
+SELECT
+    orders.orders_id,
+    orders.date_date,
+    ROUND((orders.margin + ship.shipping_fee - ship.logcost - ship.ship_cost),2) AS operational_margin
+FROM {{ ref('int_orders_margin') }} AS orders
+INNER JOIN {{ ref ('stg_gz_raw_data__ship') }} AS ship USING(orders_id)
+ORDER BY orders.orders_id DESC
