008cf9620f71fec7f2edded7dee2c1ef57b4a620
baptiste.lewagondata@gmail.com <baptiste.lewagondata@gmail.com>
2024-10-29 15:01:18 +0000

    Validated finance_days

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,19 +1,14 @@
-WITH orders AS (
-
-)
-
 SELECT
-    date_date,
-    ROUND(COUNT(orders_id),0) AS nb_transactions
-    ROUND(SUM(revenue),1) AS revenue
-    ROUND(SUM(revenue / nb_transactions),1) AS average_basket
-    ROUND(SUM(margin),1) AS margin
-    ROUND(SUM(ops.operational_margin),1) AS operational_margin
-    ROUND(SUM(purchase_cost), 1) AS purchase_cost
-    ROUND(SUM(ops.shipping_fees),1) AS total_shipping_fee
-    ROUND(SUM(ops.log_costs),1) AS total_log_costs
-    AS qty_products_sold
-FROM sales
+    orders.date_date,
+    ROUND(COUNT(orders.orders_id),0) AS nb_transactions,
+    ROUND(SUM(revenue),1) AS revenue,
+    ROUND((SUM(orders.revenue) / COUNT(orders.orders_id)),1) AS average_basket,
+    ROUND(SUM(orders.margin),1) AS margin,
+    ROUND(SUM(ops.operational_margin),1) AS operational_margin,
+    ROUND(SUM(ops.shipping_fee),1) AS total_shipping_fees,
+    ROUND(SUM(ops.logcost),1) AS total_log_costs,
+    ROUND(SUM(orders.quantity),1) AS qty_products_sold,
+FROM {{ ref('int_orders_margin') }} AS orders
 INNER JOIN {{ ref('int_orders_operational') }} AS ops USING (orders_id)
 GROUP BY date_date
 ORDER BY date_date DESC
