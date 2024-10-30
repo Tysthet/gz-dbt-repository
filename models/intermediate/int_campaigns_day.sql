8ee60f3566378afe626368b81c5ab0bc21368f3e
Baptiste Saugeron <baptiste.lewagondata@gmail.com>
2024-10-30 11:28:37 +0000

    Des trucs

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,7 +1,7 @@
 SELECT
     date_date,
-    ROUND(SUM(ads_cost),1) AS ads_cost
-    ROUND(SUM(impression),1) AS ads_impression
+    ROUND(SUM(ads_cost),1) AS ads_cost,
+    ROUND(SUM(impression),1) AS ads_impression,
     ROUND(SUM(click),1) AS ads_click
 FROM {{ ref("int_campaigns") }}
 GROUP BY date_date
