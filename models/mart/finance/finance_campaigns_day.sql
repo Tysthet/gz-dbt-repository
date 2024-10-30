f04071381ed7561ddb1fbff25bccde6b731f46e1
Baptiste Saugeron <baptiste.lewagondata@gmail.com>
2024-10-30 13:05:46 +0000

    Fixed finance_campaigns_day

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,13 +1,3 @@
-WITH e AS (
-    SELECT
-        ord.date_date,
-        ROUND(SUM(ord.purchase_cost),1) AS purchase_cost,
-        ROUND(SUM(ops.ship_cost),1) AS ship_cost
-    FROM {{ ref("int_orders_margin") }} AS ord
-    INNER JOIN {{ ref("int_orders_operational") }} AS ops
-    USING(date_date)
-    GROUP BY ord.date_date
-)
 SELECT
     f.date_date,
     ROUND(f.operational_margin - c.ads_cost,1) AS ads_margin,
@@ -18,12 +8,11 @@ SELECT
     c.ads_click,
     f.qty_products_sold,
     f.revenue,
-    e.purchase_cost,
+    f.purchase_cost,
     f.margin,
     f.total_shipping_fees,
     f.total_log_costs,
-    e.ship_cost
+    f.total_ship_costs
 FROM {{ ref("finance_days") }} AS f
 INNER JOIN {{ ref("int_campaigns_day") }} AS c USING (date_date)
-INNER JOIN e USING (date_date)
 ORDER BY f.date_date DESC
--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -3,10 +3,12 @@ SELECT
     ROUND(COUNT(orders.orders_id),0) AS nb_transactions,
     ROUND(SUM(revenue),1) AS revenue,
     ROUND((SUM(orders.revenue) / COUNT(orders.orders_id)),1) AS average_basket,
+    ROUND(SUM(orders.purchase_cost),1) AS purchase_cost,
     ROUND(SUM(orders.margin),1) AS margin,
     ROUND(SUM(ops.operational_margin),1) AS operational_margin,
     ROUND(SUM(ops.shipping_fee),1) AS total_shipping_fees,
     ROUND(SUM(ops.logcost),1) AS total_log_costs,
+    ROUND(SUM(ops.ship_cost),1) AS total_ship_costs,
     ROUND(SUM(orders.quantity),1) AS qty_products_sold,
 FROM {{ ref('int_orders_margin') }} AS orders
 INNER JOIN {{ ref('int_orders_operational') }} AS ops USING (orders_id)
